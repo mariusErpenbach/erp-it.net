44ffdb4db81682352fa1f5f80ca6644252192b0f
Hawty1 <marius_erpenbach@web.de>
2025-03-26 21:02:45 +0100

    menuBar & bannerTop

---
+++ app/ui/MenuBar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function MenuBar() {
+    return (
+      <div id="menuBar">
+        <div id="erp-it-logo">erp-it</div>
+       <section id="menuBar-btns">
+        <div id="blog-btn">Blog</div>
+        <div id="contact-btn">Contact</div>
+        <div id="procets-btn">Projects</div>
+        <div id="aboutme-btn">About Me</div>
+        </section>
+      </div>
+    );
+  }
+  
---
+++ app/ui/home/BannerTop.tsx
@@ -0,0 +1,9 @@
+export default function BannerTop() {
+  return ( 
+    <div>
+    
+        <img id="ufo" src="/ufo.svg" alt="UFO" width={50} height={50} />
+     
+    </div>
+  );
+}
