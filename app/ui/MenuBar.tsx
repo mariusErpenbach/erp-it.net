45a94ffd3a09a168650492032f266e322b2a6ed0
Hawty1 <marius_erpenbach@web.de>
2025-03-30 22:54:55 +0200

    menuBar

--- app/ui/MenuBar.tsx
+++ app/ui/MenuBar.tsx
@@ -5,10 +5,10 @@ export default function MenuBar() {
       <div id="menuBar">
         <div id="erp-it-logo">erp-it</div>
        <section id="menuBar-btns">
-        <div id="blog-btn">Blog</div>
-        <div id="contact-btn">Contact</div>
-        <div id="procets-btn">Projects</div>
-        <div id="aboutme-btn">About Me</div>
+        <a id="blog-btn">Blog</a>
+        <a id="contact-btn">Contact</a>
+        <a href="https://github.com/mariusErpenbach" target="_blank" rel="noopener noreferrer">Projects</a>
+        <a href="https://www.linkedin.com/in/marius-erpenbach-292107297/" target="_blank" rel="noopener noreferrer">About me</a>
         </section>
       </div>
     );
