619269d3d086997b261a8f2ef1bff6f000c8ada8
Hawty1 <marius_erpenbach@web.de>
2025-03-30 22:27:37 +0200

    contentSectionHome

--- app/ui/home/ContentSectionHome.tsx
+++ app/ui/home/ContentSectionHome.tsx
@@ -4,14 +4,16 @@ export default function ContentSectionHome() {
       <div id="contentSectionHome">
         <section id="webDevSection" className="contentSectionHome-section">
             <div id="webDevOverview">
-            <p><span>
-            Schnelle Ladezeiten und optimierte Assets sind essenziell für 
-             eine gute Nutzererfahrung. Ich setze auf Code-Splitting, Lazy Loading und effiziente API-Anbindungen,
+           
+            <p> <span><strong>Web-Development</strong></span> <br />
+            <strong>Schnelle Ladezeiten und optimierte Assets</strong> sind essenziell für 
+             eine gute Nutzererfahrung. Wir setzen auf <strong>Code-Splitting</strong>, <strong>Lazy Loading</strong> und <strong>effiziente API-Anbindungen</strong>,
               um reaktionsschnelle Webanwendungen zu entwickeln.
-            </span></p>
+            </p>
               </div>
                <section>
-                <div><p><span>Eine Website sollte nicht nur gut aussehen, sondern auch gefunden werden. Ich optimiere Struktur, Ladezeiten und semantisches HTML, um eine gute SEO-Performance und barrierefreie Nutzung sicherzustellen.</span></p></div>
+                <div><p>Eine <strong>Website</strong> sollte nicht nur gut aussehen, sondern auch <strong>gefunden</strong> werden. Wir optimieren <strong>Struktur</strong>, <strong>Ladezeiten</strong> und <strong>semantisches HTML</strong>, um eine gute <strong>SEO-Performance</strong> und <strong>barrierefreie Nutzung</strong> sicherzustellen.
+                </p></div>
                     <div><img src="./webDevPreview.png" alt="Three business looking People standing infront of monitor that shows a smooth looking website with some statistics on it." /></div>
                 </section>
                 <footer>
@@ -20,15 +22,11 @@ export default function ContentSectionHome() {
         </section>
         <section id="softwareDevSection"className="contentSectionHome-section">
           <div id="softwareDevOverview">
-            <h1>Software Solutions</h1>
-            <p><span>Die richtige Softwarelösung zu finden, bedeutet mehr als nur Programmieren. Es geht darum, Ihre individuellen Anforderungen genau zu verstehen und gemeinsam eine Lösung zu entwickeln, die perfekt auf Ihr Unternehmen abgestimmt ist.
-Wir arbeiten eng zusammen, um Software zu schaffen, die nicht nur funktioniert, sondern Ihr Unternehmen effizienter, produktiver und zukunftssicher macht. Von der ersten Idee bis zur finalen Umsetzung – gemeinsam bringen wir Ihre Vision zum Leben.
-</span></p>
-          
-          <div><p>lets plan together</p> <a href="#">contact me</a></div>  
+            <p><span><strong>Software-Solutions</strong></span><br />
+            Mehr als <strong>Programmieren</strong> – wir entwickeln <strong>maßgeschneiderte Software</strong>, die Ihr Unternehmen <strong>effizienter</strong> und <strong>zukunftssicher</strong> macht. Von der Idee bis zur Umsetzung</p>
           </div>
-          <footer>
-            <img src="./alienPlanning.png" alt="Three business aliens having an idea. They sitting together at a desk with many strange devices. They look friendly. Photo is very cartoonish. " />
+          <section>
+          <img src="./alienPlanning.png" alt="Three business aliens having an idea. They sitting together at a desk with many strange devices. They look friendly. Photo is very cartoonish. " />
             <div className="alienPresentationStep">
             <p>Plan</p>
             <i>➡</i>
@@ -44,12 +42,25 @@ Wir arbeiten eng zusammen, um Software zu schaffen, die nicht nur funktioniert, 
             <i>➡</i>
             </div>
             <img src="./alienCelebration.png" alt="Two business aliens and a coder alien celebrating together the release of the new software infront of an big present. All are very happy. cartoonish style." />
-          </footer>
+          </section>
+         <footer>
+            <h1>Lets connect and start a project</h1>
+            <form action="submit">
+              <textarea name="your idea" id="" placeholder="Deine Idee"></textarea>
+              <div><input type="text" placeholder="Dein Kontakt"/> <button>Senden</button></div>
+            </form>
+          </footer> 
         </section>
-        <section className="contentSectionHome-section">
-          <div>
-            <h1>Embedded Software</h1>
-            <p></p>
+        <section id="embeddedSoftwareSection"className="contentSectionHome-section">
+          <div id="embeddedSoftwareOverview">
+            <p><span><strong>Embedded Software</strong></span><br />
+            <strong>Arduino</strong>- und <strong>Raspberry Pi</strong>-Mikrocontroller-Projekte für den <strong>Smart-Home</strong>-Bereich. Durch den Einsatz von <strong>Relais</strong> lassen sich verschiedene Geräte steuern und automatisieren – von <strong>Beleuchtungssystemen</strong> über <strong>Heizungsregelungen</strong> bis hin zu intelligenten <strong>Stromzählersystemen</strong> zur <strong>Verbrauchsüberwachung</strong>. <strong>Sensoren</strong> und <strong>drahtlose Kommunikation</strong> ermöglichen zudem eine nahtlose Integration in bestehende Systeme. So entsteht ein <strong>komfortables</strong> und <strong>energieeffizientes</strong> Zuhause, das sich an individuelle Bedürfnisse anpasst.</p>
+          </div>
+          <div id="embeddedSoftwarePics">
+              <img src="./ES1.jpg" alt="" />
+           
+              <img src="./ES3.jpg" alt="" />
+              <img src="./ES2.jpg" alt="" />
           </div>
         </section>
       </div>
