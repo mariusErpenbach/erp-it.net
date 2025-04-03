c15f6504def0799aaa2ba97c5b1612dfaede5a96
Hawty1 <marius_erpenbach@web.de>
2025-04-04 00:07:02 +0200

    mongodb

--- app/db/mongoDB.js
+++ app/db/mongoDB.js
@@ -1,21 +1,17 @@
-require("dotenv").config();
-const { MongoClient } = require("mongodb");
-
-// URI aus der .env-Datei holen
-const uri = process.env.MONGODB_URI;
-
-const client = new MongoClient(uri);
+const client = new MongoClient(process.env.MONGO_URI);
 
 async function connectDB() {
   try {
     await client.connect();
     console.log("✅ Erfolgreich mit MongoDB Atlas verbunden!");
-    const db = client.db("erpnetDB");
+
+    // Beispiel: Greife auf eine Datenbank zu
+    const db = client.db("erpnetDB"); 
     return db;
   } catch (error) {
     console.error("❌ Fehler beim Verbinden mit MongoDB:", error);
   }
 }
-
 connectDB();
 module.exports = connectDB;
+
