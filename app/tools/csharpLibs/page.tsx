09c79c816cf7f3c889be5387046eb2e45046807c
Hawty1 <marius_erpenbach@web.de>
2025-04-15 13:20:54 +0200

    csharp libs tools page

---
+++ app/tools/csharpLibs/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import CSharpStandardLibs from "@/app/ui/tools/csharpStandardLibs"
+
+
+export default function csharpPage(){
+    return(
+        <div>
+       <CSharpStandardLibs></CSharpStandardLibs>
+        </div>
+    )
+}
